9e6e8d355b831dc163d65cc961af834c37f4839f
KaungThant-Zin <mrxray261@gmail.com>
2021-09-03 23:06:17 +0630

    bgcolor and asset image

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,13 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Image(
+          image: AssetImage('images/diamond.png'),
         ),
       ),
     ),
